4849f7794eb0671d46e46a9224c2feb1c9092964
leonardogloria <A5B7alfazulu>
2016-09-27 06:22:39 -0300

    Gestao de Tasks

---
+++ grails-app/controllers/beowulf/TaskController.groovy
@@ -0,0 +1,125 @@
+package beowulf
+
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+@Secured('ROLE_USER')
+
+class TaskController {
+    def springSecurityService
+
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Task.list(params), model:[taskCount: Task.count()]
+    }
+
+    def show(Task task) {
+        respond task
+    }
+
+    def create(Project project) {
+        respond new Task(params),model:[project:project]
+    }
+
+    @Transactional
+    def save(Task task) {
+        def _loggedUser = springSecurityService.getPrincipal()
+        def _user = User.findByUsername(_loggedUser.username)
+        def _project = Project.get(params.projectId)
+        if (task == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+        if(task.assignedTo){
+            task.status = Status.ASSIGNED
+        }
+        task.createdDate = new Date()
+        task.openBy = _user
+        task.project = _project
+        _project.addToTasks(task)
+
+        if (!task.validate()) {
+            transactionStatus.setRollbackOnly()
+            respond task.errors, view:'create', model:[project: _project]
+            return
+        }
+
+        //task.save flush:true, failOnError:true
+        _project.save(flush:true,failOnError:true)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'task.label', default: 'Task'), task.id])
+                redirect controller: 'project', action: 'dashboard', id: task.project.id
+            }
+            '*' { respond task, [status: CREATED] }
+        }
+    }
+
+    def edit(Task task) {
+        respond task, model:[project: task.project]
+    }
+
+    @Transactional
+    def update(Task task) {
+        if (task == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (!task.validate()) {
+            transactionStatus.setRollbackOnly()
+            respond task.errors, view:'edit'
+            return
+        }
+
+        task.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'task.label', default: 'Task'), task.id])
+                redirect controller: 'project', action: 'dashboard', id: task.project.id
+            }
+            '*'{ respond task, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Task task) {
+
+        if (task == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        task.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'task.label', default: 'Task'), task.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'task.label', default: 'Task'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/views/task/create.gsp
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="layout" content="main" />
+        <g:set var="entityName" value="${message(code: 'task.label', default: 'Task')}" />
+        <title> <g:message message="${entityName}" /></title>
+    </head>
+    <body>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                <g:message message="${entityName}" />
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i>  <g:message message="${entityName}" /></a></li>
+                <li class="active">Novo</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Novo  <g:message message="${entityName}" /></h3>
+
+                </div>
+                <div class="box-body">
+
+                    <g:hasErrors bean="${this.task}">
+                        <ul class="errors" role="alert">
+                            <g:eachError bean="${this.task}" var="error">
+                                <li <g:if test="${error in org.springframework.validation.FieldError}">data-field-id="${error.field}"</g:if>><g:message error="${error}"/></li>
+                            </g:eachError>
+                        </ul>
+                    </g:hasErrors>
+
+                    <g:form action="save"  class="form-horizontal" role="form">
+                        <fieldset class="form">
+                            <g:render template="form" ></g:render>
+
+                        </fieldset>
+                        <fieldset class="buttons">
+                            <g:submitButton name="create" class="btn btn-primary" value="${message(code: 'default.button.create.label', default: 'Create')}" />
+                        </fieldset>
+                    </g:form>
+
+                </div>
+            </div>
+        </section>
+    </div>
+
+    </body>
+</html>
